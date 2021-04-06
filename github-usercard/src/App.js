c83d0c92ed90d1682e0dd9949a00ae3517764b8f
Austin Greer <agreer.ag12@gmail.com>
2021-04-06 15:30:10 -0500

    when component mounts, successfully receiving user data from api and setting to state

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    user: '',
+    followers: ''
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/AustinGreer')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        user:res.data
+      })
+    })
+    .catch(err => console.log('error here', err))
+  }
+
   render() {
     return (
       <div>
